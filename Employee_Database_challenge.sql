6a3a00b3a0f62680793036c26f9e7a0fb3da0372
Farbod_Jambor <farbodjambor@gmail.com>
2021-10-17 15:15:23 -0700

    working through deliverable 1 challenge

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,4 +1,3 @@
-
 -- Challenge - Deliverable 1
 SELECT e.emp_no,
 	   e.first_name,
@@ -12,3 +11,21 @@ INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+				   first_name,
+				   last_name,
+ 				   title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no,
+to_date DESC;
+
+-- Retrieve the number of employees by their most recent job title who are about to retire.
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
