0728bee7af6261d093aba235a81140eebc3b50a5
Farbod_Jambor <farbodjambor@gmail.com>
2021-10-16 18:29:17 -0700

    modifying titles table

--- schema.sql
+++ schema.sql
@@ -36,27 +36,39 @@ CREATE TABLE titles	 (
      title VARCHAR NOT NULL,
      from_date DATE NOT NULL,
      to_date DATE NOT NULL,
-     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-     PRIMARY KEY (title)
+	 PRIMARY KEY (emp_no,title)
+	 
 );
+
+
+
 CREATE TABLE dept_emp	 (
 	 emp_no INT NOT NULL,
      dept_no VARCHAR (4) NOT NULL,
      from_date DATE NOT NULL,
      to_date DATE NOT NULL,
-     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
      PRIMARY KEY (emp_no,dept_no)
 );
+
+
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')  
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
 -- Number of employees retiring
 SELECT COUNT(first_name)
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
--- Retirement eligibility
 SELECT first_name, last_name
 INTO retirement_info
 FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')  
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT *
+from retirement_info
